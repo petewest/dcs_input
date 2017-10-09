49f33514c2681151384c9c67ad6554766d5bd8b6
petewest <github@petewest.org>
2017-10-09 21:57:33 +0100

    Unbind from keypad

---
+++ SA342/keyboard/Keyboard.diff.lua
@@ -0,0 +1,51 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3003pnilu3004cd8vd1vpnilvu0"] = {
+			["name"] = "Trim: Nose Down",
+			["removed"] = {
+				[1] = {
+					["key"] = ";",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+		},
+		["d3009pnilu3010cd8vd1vpnilvu0"] = {
+			["name"] = "Trim: Left Wing Down",
+			["removed"] = {
+				[1] = {
+					["key"] = ",",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+		},
+		["d3029pnilu3030cd7vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Z",
+				},
+			},
+			["name"] = "Autopilot Master",
+		},
+		["dnilp201u202cdnilvdnilvpnilvunil"] = {
+			["name"] = "Rudder left",
+			["removed"] = {
+				[1] = {
+					["key"] = "Z",
+				},
+			},
+		},
+		["dnilp203u204cdnilvdnilvpnilvunil"] = {
+			["name"] = "Rudder right",
+			["removed"] = {
+				[1] = {
+					["key"] = "X",
+				},
+			},
+		},
+	},
+}
+return diff
